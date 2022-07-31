e0d7ae92d22e99480a266e33cd0b09388e2349aa
thuannguyendeveloper <49790787+thuannguyendeveloper@users.noreply.github.com>
2022-07-31 15:56:20 +0700

    Init projects

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by NNT on 31/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
